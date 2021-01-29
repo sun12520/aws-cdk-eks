60561277ce122305f3d523f32be89cce1eed15bc
EC2 Default User <ec2-user@ip-172-31-17-206.ap-southeast-1.compute.internal>
2021-01-29 08:26:52 +0000

    add file

---
+++ dockerAssets.d/image_scan.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+ECR_REPO_URI=$1
+IMGTAG=$2
+AWS_DEFAULT_REGION=$3
+
+IFS=$'\n'
+LEVEL=['MEDIUM','High','Critical']
+# cmd="/usr/bin/aws ecr describe-image-scan-findings --repository-name $ECR_REPO_URI --image-id imageTag=$IMGTAG --region $AWS_DEFAULT_REGION --query imageScanFindings --output=text"
+cmd="df"
+# echo $cmd
+for line in `aws ecr describe-image-scan-findings --repository-name $ECR_REPO_URI --image-id imageTag=$IMGTAG --region $AWS_DEFAULT_REGION --query imageScanFindings --output=text`
+# for line in `$cmd`
+do
+    # echo $line
+    level=`echo $line| grep 'FINDINGS' | awk -F " " '{print $3}'`
+    if [ "$level" == 'MEDIUM' ] || [ $level == 'High' ] || [ $level == 'Critical' ];then
+        echo 'ERROR'
+        exit 1
+    fi
+    echo 'pass'
+done
