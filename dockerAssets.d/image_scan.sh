517479a71d378c425c7b2a32180e802198572511
EC2 Default User <ec2-user@ip-172-31-17-206.ap-southeast-1.compute.internal>
2021-01-29 11:15:31 +0000

    modify

--- dockerAssets.d/image_scan.sh
+++ dockerAssets.d/image_scan.sh
@@ -6,21 +6,18 @@ IMGTAG=$2
 AWS_DEFAULT_REGION=$3
 
 IFS=$'\n'
-LEVEL=['MEDIUM','High','Critical']
+#LEVEL=['MEDIUM','High','Critical']
 # cmd="/usr/bin/aws ecr describe-image-scan-findings --repository-name $ECR_REPO_URI --image-id imageTag=$IMGTAG --region $AWS_DEFAULT_REGION --query imageScanFindings --output=text"
-cmd="df"
-# echo $cmd
+
 for line in `aws ecr describe-image-scan-findings --repository-name $ECR_REPO_URI --image-id imageTag=$IMGTAG --region $AWS_DEFAULT_REGION --query imageScanFindings --output=text`
 # for line in `$cmd`
 do
-    # echo $line
-    if [ `echo $line | grep 'FINDINGS'` != '' ];then
+    if [[ `echo $line | grep 'FINDINGS'` != '' ]];then
         level=`echo $line | awk -F " " '{print $3}'`
         echo $level
-        if [ $level == 'High' ] || [ $level == 'Critical' ] || [ $level == 'MEDIUM' ] ;then
+        if [[ $level = 'High' ]] || [[ $level = 'Critical' ]] ;then
             echo 'ERROR'
             exit 1
         fi
     fi
-    # echo 'pass'
 done
